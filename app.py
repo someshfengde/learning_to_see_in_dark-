938bb48976efbfd0fb19a7b95ed6cbc212dabe11
someshfengde <someshfengde@gmail.com>
2021-07-13 21:22:35 +0530

    ADED X

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ readme_text = st.markdown(get_file_content_as_string("README.md"))
 
 expander =  st.beta_expander(label='sample images',expanded=True)
 
-with expander:
+with expander as x:
     col1, col2 = st.beta_columns(2)
-    # col1.write('dark image input')
     c1 = col1.markdown("<h2 style='text-align: center; color: gray;'>Dark image input</h1>", unsafe_allow_html=True)
     c2 = col2.markdown("<h2 style='text-align: center; color: gray;'>Enhanced image</h1>", unsafe_allow_html=True)
     i1 = col1.image('https://github.com/someshfengde/learning_to_see_in_dark/raw/main/images/input1.png' ,use_column_width=True)
@@ -25,7 +24,7 @@ if app_mode == "Show instructions":
     st.sidebar.success('To continue select "Run the app".')
 elif app_mode == "Show the source code":
     readme_text.empty()
-    expander.empty()
+    x.empty()
     i1.empty()
     i2.empty()
     i3.empty()
